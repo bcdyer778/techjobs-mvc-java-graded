9b2b681f05f35eb29bc69c2190d086e35787d513
bcdyer778 <99931077+bcdyer778@users.noreply.github.com>
2022-11-14 21:00:57 -0600

    testing

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -28,7 +28,7 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping(value = "results")
+    @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam(value="searchType") String searchType, @RequestParam(value="searchTerm") String searchTerm){
         model.addAttribute("columns", columnChoices);
         model.addAttribute("searchType", searchType);
